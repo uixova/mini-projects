970c652ef4ea79ad0961d615258ed813b4ba1d70
salih <uixova.studio@gmail.com>
2025-12-08 15:28:06 +0300

     

---
+++ Consulting-Coaching/assets/js/main.js
@@ -0,0 +1,14 @@
+const sections = document.querySelectorAll('section');
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting){
+                entry.target.classList.add('active');
+            };
+        });
+    },
+    {threshold: 0.5}
+);
+
+sections.forEach(section => observer.observe(section));
+
